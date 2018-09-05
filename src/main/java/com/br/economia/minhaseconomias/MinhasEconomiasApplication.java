809932607721e0249874e0afed04c216560f98dc
Gabriel Moreira Carlos <gabrielmcarlos@hotmail.com>
2018-09-05 19:02:35 -0300

    Projeto criado

---
+++ src/main/java/com/br/economia/minhaseconomias/MinhasEconomiasApplication.java
@@ -0,0 +1,12 @@
+package com.br.economia.minhaseconomias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasEconomiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhasEconomiasApplication.class, args);
+	}
+}
---
+++ src/test/java/com/br/economia/minhaseconomias/MinhasEconomiasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.economia.minhaseconomias;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinhasEconomiasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
